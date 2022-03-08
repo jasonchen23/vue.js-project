7a28a0c7a00fa83237dd4e6bf4b15b5679e16b6f
Jasonchen23 <kevin3121961@gmail.com>
2022-03-09 00:20:20 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <NavbarComponent />
+  <router-view/>
+</template>
+<script>
+import NavbarComponent from '@/components/NavbarComponent.vue'
+export default {
+  components:{
+    NavbarComponent
+  }
+}
+</script>
+<style>
+  body{
+    margin: 0px;
+  }
+</style>
