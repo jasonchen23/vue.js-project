79e617e9b06226ed4f0b4e4b568547f69dd3f0c4
Jasonchen23 <kevin3121961@gmail.com>
2022-03-09 00:21:47 +0800

    add router index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import ProductsView from '../views/ProductsView.vue'
+import ProductView from '../views/ProductView.vue'
+import CheckoutView from '../views/CheckoutView.vue'
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/products',
+    name: 'products',
+    component: ProductsView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/checkout',
+    name: 'checkout',
+    component: CheckoutView
+  },
+  {
+    path: '/products/:productId',
+    name: 'product',
+    component: ProductView
   }
+  
 ]
 
 const router = createRouter({
