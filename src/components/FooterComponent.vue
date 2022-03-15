68ca5dbfc48ceeac39af67731bb3ec7c9744c9d0
HsuBoCheng <95022923+HsuBoCheng@users.noreply.github.com>
2022-03-15 15:00:40 +0800

    update footer 排版

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,29 +1,30 @@
 <template>
   <div class="information row d-flex align-items-end">
-    <div class="col-lg-3 text-center">
+    <div class="col-2"></div>
+    <div class="col-lg-2 text-center">
       <img src="./images/bugerLogo.svg" alt="" class="logo" />
     </div>
-    <div class="info col-lg-4 mb-lg-2 text-lg-start text-center">
+    <div class="info col-lg-2 mb-lg-2 text-lg-start text-center">
       <h6><span>Company</span> 漢堡女王股份有限公司 </h6>
       <h6><span>Telephone</span> 00-360-3000</h6>
       <h6><span>Service Time</span> 09:00 - 17:00</h6>
       <h6><span>Add</span> 台北市中正區龍山街206號</h6>
       <h6><span>Email</span> bugerqueen@dmail.com</h6>
     </div>
-    <div class="col-lg-5 text-lg-start text-center mb-lg-2">
-      <i
-        ><p><span>加盟專線</span> 0800-002-003</p></i
-      >
-      <logo class="text-center d-flex justify-content-center me-5">
+    <div class="col-lg-4 text-center mb-lg-2">
+      <i><span>加盟專線</span> 0800-002-003</i>
+      &nbsp;
+      <logo> 
         <a href="https://zh-tw.facebook.com/"
           ><img class="infoLink" src="./images/facebook.svg"
         /></a>
-        &nbsp;&nbsp;
+        &nbsp;
         <a href="https://www.instagram.com"
           ><img class="infoLink" src="./images/instagram.svg" alt=""
         /></a>
       </logo>
     </div>
+    <div class="col-lg-2"></div>
   </div>
 </template>
 <style scoped>
